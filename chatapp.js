45c46dcaf95bd7a4ca477c2d2a2e68dfc87702e6
digi360 <sydney.sedibe@umuzi.org>
2018-09-12 11:53:23 +0200

    Web project started

---
+++ chatapp.js
@@ -0,0 +1,98 @@
+<<<<<<< HEAD
+const express = require('express')
+const app = express()
+
+
+//set the template engine ejs
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.static('public'))
+
+
+//routes
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+//Listen on port 3000
+server = app.listen(3000)
+
+
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected: ' + socket.client.conn.readyState.toString());
+
+	//default username
+	socket.username = "User1";
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+=======
+const express = require('express')
+const app = express()
+
+
+//set the template engine ejs
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.static('public'))
+
+
+//routes
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+//Listen on port 3000
+server = app.listen(3000)
+
+
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected: ' + socket.client.conn.readyState.toString());
+
+	//default username
+	socket.username = "User1";
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+>>>>>>> 85872d41b07fcb66432f0e20bb30603dac077385
+})
